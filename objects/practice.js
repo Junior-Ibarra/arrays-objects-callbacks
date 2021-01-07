b267d52e01ab26066370a56a51f5cdd07e931a5a
Junior Ibarra <Ibarra_0811@yahoo.com>
2021-01-07 14:58:19 -0700

    First Commit

--- objects/practice.js
+++ objects/practice.js
@@ -12,7 +12,7 @@
   Create an object called me.
   Give it a key of name with the value being your name, and another key of age with the value being your age.
   Then alert your name using dot notation.
-*/
+*/ob
 
 //Code here
 
